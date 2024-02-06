13aa174250cb11e91e2b9c1d9755b312b208df7a
Aleksvtop <vazilino@gmail.com>
2024-02-06 14:54:38 +0300

    Change APP component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+//import { Accordion } from './Components/Accordion';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+const Rating = () => {
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+const Star = () => {
+    return (
+        <>
+            <div>star</div>
+        </>
+    )
+}
+
+const Accordion = () => {
+    return (
+        <>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </>
+    )
+}
+
+const AccordionTitle = () => {
+    return (
+        <h3>Menu</h3>
+    )
+}
+
+const AccordionBody = () => {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
 }
 
 export default App;
